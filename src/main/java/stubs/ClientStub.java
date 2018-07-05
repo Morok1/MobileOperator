1121f7f0f9354d6da99f32b0965376c0ab4e97ba
evgenij <zhenyasmagin@yandex.ru>
2018-07-05 12:46:07 +0300

    Add all function of stub  client

--- src/main/java/stubs/ClientStub.java
+++ src/main/java/stubs/ClientStub.java
@@ -2,7 +2,9 @@ package stubs;
 
 import model.Client;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class ClientStub {
     /*
@@ -21,13 +23,46 @@ public class ClientStub {
     private String password;
 
     * */
-    Client client = new Client();
+
     public Client initOfOneClient(){
-        client.setName("Vasya");
+        Client client = new Client();
+        String name = "Vasya";
+        setProperties(client, name);
+        return  client;
+    }
+
+    public List<Client> initOfListsClients(){
+        Client client1 = new Client();
+        Client client2 = new Client();
+        Client client3 = new Client();
+        Client client = new Client();
+
+        String name = "Vasya";
+        String name1 = "Petya1";
+        String name2 = "Petya2";
+        String name3 = "Petya3";
+
+        setProperties(client, name);
+        setProperties(client1, name1);
+        setProperties(client2, name2);
+        setProperties(client3, name3);
+
+        List<Client> clientList = new ArrayList<Client>();
+
+        clientList.add(client);
+        clientList.add(client1);
+        clientList.add(client2);
+        clientList.add(client3);
+        return null;
+    }
+
+
+    void setProperties(Client client, String name) {
+        client.setName(name);
         client.setLastName("Petrov");
         client.setBirthDay(new Date(44444444));
         client.setAddress("Moscow");
         client.setEmail("ui@mail.ru");
-        return  client;
     }
+
 }
