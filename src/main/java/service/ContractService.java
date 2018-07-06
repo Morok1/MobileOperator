6e5932b1d44f615e6fa0b6e94aa6aed1a33905ee
evgenij <zhenyasmagin@yandex.ru>
2018-07-06 08:31:18 +0300

    Add contractService and optinonService

---
+++ src/main/java/service/ContractService.java
@@ -0,0 +1,17 @@
+package service;
+
+import model.Contract;
+import org.apache.log4j.Logger;
+import repositoryImplementation.ContractRepositoryImpl;
+
+
+
+public class ContractService  {
+    private ContractRepositoryImpl contractRepository;
+    private static Logger logger = Logger.getLogger(ContractService.class);
+
+    public Contract getContractWithNumber(int number) {
+        logger.info("Start function:" + "getContractWithNumber");
+        return contractRepository.getContractWithNumber(number);
+    }
+}
--- src/main/java/service/OptionService.java
+++ src/main/java/service/OptionService.java
@@ -1,21 +1,23 @@
 package service;
 
 import model.Option;
+import org.apache.log4j.Logger;
 import repositoryImplementation.OptionRepositoryImpl;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class OptionService {
+    private static Logger logger = Logger.getLogger(OptionService.class);
+    private OptionRepositoryImpl optionRepository = new OptionRepositoryImpl();
 
-    OptionRepositoryImpl optionRepository
     public Option getOptionWithName(String name) {
-
-        return option;
+        logger.info(name);
+        return optionRepository.getOptionWithName(name);
     }
 
     public List<Option> getOptionWithCostOverLimit(int limit) {
-
-        return optionList;
+        logger.info(limit);
+        return optionRepository.getOptionWithCostOverLimit(limit);
     }
 }
